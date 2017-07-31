77ab1dc677642a7700dd07217c3dc80a7d41353a
Kai Ye <yekai1024@gmail.com>
2017-07-31 23:59:45 +0800

    init change

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,6 +16,7 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -25,7 +26,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+### MY test code ###
 
 
 #########################################################
