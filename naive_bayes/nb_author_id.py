5a6a905565b63a2e7b2d9858162ade96a52e6ed8
Kai Ye <yekai1024@gmail.com>
2017-08-01 14:04:59 +0800

    add SVM support

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,14 +15,28 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn.naive_bayes import GaussianNB
-
+from sklearn.svm import SVC
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#clf = SVC(kernel="linear")
+"""
+SVC is quite slow compare to GNB, but a bit higher accuracy
+training time: 125.967 s
+predict time: 13.041 s
+accuracy:  0.984072810011
+"""
+
 clf = GaussianNB()
+"""
+training time: 0.592 s
+predict time: 0.077 s
+accuracy:  0.973265073948
+"""
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
